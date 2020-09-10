d3da052688d7549bb7f4339eb0cf34761334d102
Nishant Gupta <nishant.rajvanshi17@gmail.com>
2020-09-10 20:50:28 +0530

    Add files via upload

---
+++ code/gui/classifier.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# @Author: Prateek Sachan
+# @Date:   2017-02-24 16:55:08
+# @Last Modified by:   Prateek Sachan
+# @Last Modified time: 2017-03-24 09:33:12
+
+
+def cal(classes):
+        image_size = 500
+        num_channels = 3
+        train_classes = classes
+        print("Classification running")
+        print(classes)
+        import time
+        time.sleep(10)
+        final_accuracy = 70
